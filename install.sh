8d79d1fc8a1692d7b7a393b026d3acbf0cf0a42e
Harry-zklcdc <1269158832@qq.com>
2023-08-18 12:40:24 +0800

    [Add] 🎉 Cloudflare Tunnel & Fix Format

--- install.sh
+++ install.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-if [ "$TYPE" == "HIPER" ]; then
+Install_Hiper(){
   wget -q https://gitcode.net/to/hiper/-/raw/master/linux-amd64/hiper -O /usr/local/bin/hiper
   chmod +x /usr/local/bin/hiper
   sudo hiper -service install -config $HIPER_AUTH_TOKEN
   sudo hiper -service start
   echo Setting Up Hiper
-elif [ "$TYPE" == "NGROK" ]; then
+}
+
+Install_CFT(){
+  curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+  sudo dpkg -i cloudflared.deb && 
+  sudo cloudflared service install $CFT_AUTH_TOKEN
+}
+
+Install_Ngrok(){
   wget -q https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
   tar -xzf ngrok-v3-stable-linux-amd64.tgz
   chmod +x ./ngrok
   ./ngrok config add-authtoken $NGROK_AUTH_TOKEN
   ./ngrok http 8080
   echo Setting Up Ngrok
-elif [ "$TYPE" == "FRP" ]; then
-  if [ "$FRP_TYPE" == "SAKURA" ]; then
-    wget -q https://nya.globalslb.net/natfrp/client/frpc/0.45.0-sakura-7/frpc_linux_amd64
-    mv ./frpc_linux_amd64 ./frpc && chmod +x ./frpc
-    sudo ./frpc -f $FRP_AUTH_TOKEN
-  else
-    wget -q https://github.com/fatedier/frp/releases/download/v0.51.3/frp_0.51.3_linux_amd64.tar.gz
-    tar -xzf frp_0.51.3_linux_amd64.tar.gz
-    mv ./frp_0.51.3_linux_amd64/frpc ./frpc && chmod +x ./frpc
-    cat >./frpc.ini<<EOF
+}
+
+Install_Frp_SAKURA(){
+  wget -q https://nya.globalslb.net/natfrp/client/frpc/0.45.0-sakura-7/frpc_linux_amd64
+  mv ./frpc_linux_amd64 ./frpc && chmod +x ./frpc
+  sudo ./frpc -f $FRP_AUTH_TOKEN
+  echo Setting Up Frp SAKURA
+}
+
+Install_Frp_OTHER(){
+  FRP_FILE_URL=$(curl -s https://api.github.com/repos/fatedier/frp/releases/latest | grep linux_amd64 | grep browser_download_url | cut -d '"' -f 4)
+  wget -q ${FRP_FILE_URL} -O frp.tar.gz
+  tar -xzf frp.tar.gz
+  mv ./frp_0.51.3_linux_amd64/frpc ./frpc && chmod +x ./frpc
+  cat >./frpc.ini<<EOF
 [common]
 server_addr = $FRP_ADDRESS
 server_port = $FRP_PORT
@@ -36,10 +49,11 @@ plugin_local_addr = 127.0.0.1:8080
 plugin_host_header_rewrite = 127.0.0.1
 plugin_header_X-From-Where = frp
 EOF
-    sudo ./frpc -c ./frpc.ini
-  fi
-  echo Setting Up Frp
-elif [ "$TYPE" == "NPS" ]; then
+  sudo ./frpc -c ./frpc.ini
+  echo Setting Up Frp Other
+}
+
+Install_NPS(){
   wget -q https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz
   tar -xzf linux_amd64_client.tar.gz
   chmod +x ./npc
@@ -47,4 +61,20 @@ elif [ "$TYPE" == "NPS" ]; then
   sudo rm -rf linux_amd64_client.tar.gz conf
   sudo npc start
   echo Setting Up NPS
+}
+
+if [ "$TYPE" == "HIPER" ]; then
+  Install_Hiper
+elif [ "$TYPE" == "CFT" ]; then
+  Install_CFT
+elif [ "$TYPE" == "NGROK" ]; then
+  Install_Ngrok
+elif [ "$TYPE" == "NPS" ]; then
+  Install_NPS
+elif [ "$TYPE" == "FRP" ]; then
+  if [ "$FRP_TYPE" == "SAKURA" ]; then
+    Install_Frp_SAKURA
+  else
+    Install_Frp_OTHER
+  fi
 fi
